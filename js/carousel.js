5029cc59842686ce24afde0453883bd898339155
Степан <bilenkuy7440@gmil.com>
2021-07-31 01:04:47 +0300

    css

--- js/carousel.js
+++ js/carousel.js
@@ -1,25 +1,51 @@
-const slides = `[productsJson]`; 
+(function() {
 
-let currentSlide = 0;
-function showCurrentSlide() {
-    const slideTarget = document.querySelector('.carousel-two"')
-    slideTarget.src = slides[currentSlide];
-}
+    const slides = [
+      '<div><img src="carwash" alt="Cat"><h2>Cat 1</h2></div>',
+      <img src="img/carwash.jpg" alt="CarWash" />
 
-function nextSlide() {
-    currentSlide++;
-    if (currentSlide>= slides.length) currentSlide=0;
-    showCurrentSlide();
-}
-
-function prevSlide() {
-    currentSlide--;
-    if (currentSlide<0) currentSlide=slides.length-1;
-    showCurrentSlide();
-}
-
-document.querySelector('.bnext').addEventListener('click', nextSlide);
-document.querySelector('.bprev').addEventListener('click', nextSlide);
-
-showCurrentSlide();
-setInterval(nextSlide, 3000);
+    ];
+  
+    let firstSlide = 0;
+    let slidesToShow = 6;
+  
+    function showCurrentSlide() {
+      const slidesContainer = document.querySelector('.slides');
+      let slideIdx = firstSlide;
+      let html = '';
+      for (let i = 1; i <= slidesToShow; i++) {
+        html += slides[slideIdx];
+        slideIdx = slideIdx + 1 >= slides.length ? 0 : slideIdx + 1;
+      }
+      slidesContainer.innerHTML = html;
+    }
+  
+    function nextSlide() {
+      firstSlide = firstSlide + 1 >= slides.length ? 0 : firstSlide + 1;
+      showCurrentSlide();
+    }
+  
+  
+    setInterval(nextSlide, 2000);
+  
+    function resize() {
+      console.log(window.screen.width);
+      if (window.screen.width <= 400) {
+        slidesToShow = 1;
+      } else if (window.screen.width <= 600) {
+        slidesToShow = 2;
+      } else if (window.screen.width <= 800) {
+        slidesToShow = 3;
+    } else if (window.screen.width <= 1000) {
+        slidesToShow = 4;
+    } else if (window.screen.width <= 1200) {
+        slidesToShow = 5;
+      } else {
+        slidesToShow = 6;
+      }
+      showCurrentSlide();
+    }
+   
+    window.addEventListener('resize', resize);
+  
+  })();
